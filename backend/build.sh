91d32081865293468d81fbbeaa425856201097a9
mgiftson0 <mgiftson00@gmail.com>
2025-09-01 20:03:30 +0000

    changes

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install --upgrade setuptools wheel
+pip install --upgrade pip setuptools wheel
 pip install flask==2.3.3 flask-cors==4.0.0 qrcode==7.4.2 gunicorn==21.2.0
-pip install Pillow==10.3.0
+pip install --only-binary=Pillow "Pillow>=10.4.0"
