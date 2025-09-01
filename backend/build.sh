07fd5c9fb11240c7ba7c283d9a7b533c68aa5fdb
mgiftson0 <mgiftson00@gmail.com>
2025-09-01 18:26:11 +0000

    .

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install --upgrade setuptools wheel
-pip install flask==2.3.3 flask-cors==4.0.0 qrcode[pil]==7.4.2 gunicorn==21.2.0
+pip install flask==2.3.3 flask-cors==4.0.0 qrcode==7.4.2 gunicorn==21.2.0
 pip install Pillow==10.3.0
